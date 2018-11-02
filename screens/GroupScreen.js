c29c7149dd85754568960c068b260e55bfefb7ae
gallagti <31393304+gallagti@users.noreply.github.com>
2018-11-02 11:47:17 -0700

    Version 1.0.2
    ~ Additional home screen upgrades with new logos and buttons

--- screens/GroupScreen.js
+++ screens/GroupScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Container, Header, Content, Button, Text } from 'native-base';
 import {
   Image,
   Platform,
   ScrollView,
   StyleSheet,
-  Text,
   TouchableOpacity,
   View,
 } from 'react-native';
@@ -13,7 +14,8 @@ import {
 import { WebBrowser } from 'expo';
 import { MonoText } from '../components/StyledText';
 import settingIcon from '../assets/images/settingIcon.png';
-import boxIcon from '../assets/images/transparentBox.png';
+import boxIcon from '../assets/images/SupportBoxMainLogoTranUpdated.png';
+import groupPic from '../assets/images/hands.png'
 
 export default class MyGroupsScreen extends React.Component {
   static navigationOptions = {
@@ -22,41 +24,75 @@ export default class MyGroupsScreen extends React.Component {
 
   render() {
        return (
-           <ScrollView style={styles.container}>
-               <View>
+          <ScrollView style={styles.container}>
                    <View style={styles.BoxIcon}>
+
                        <Image
                            style={styles.BoxIcon}
-                           source={require('../assets/images/transparentBox.png')}
+                           source={require('../assets/images/SupportBoxMainLogoTranUpdated.png')}
                        />
+
+                   </View>
+
+                   <View>
+                      
+                      <Content>
+                        <Button block style = {styles.MyGroupsButton}>
+                          <Text>My Groups</Text>
+                        </Button>
+                        <Button block style = {styles.JoinAGroupButton}>
+                          <Text>Join a Group</Text>
+                        </Button>
+                        <Button block style = {styles.SearchAGroupButton}>
+                          <Text>Search for a Group</Text>
+                        </Button>
+                      </Content>
                    </View>
                    <View>
-                      <Text style={styles.MyGroupsText}>SupportBox</Text>
-                   </View>
+
                </View>
+
            </ScrollView>
+
        )
    };
  }
+
+
 
    const styles = StyleSheet.create({
      container: {
        flex: 1,
        backgroundColor: '#57caff',
      },
+     MyGroupsButton: {
+       marginBottom: 15,
+     },
+     JoinAGroupButton: {
+       marginBottom: 15,
+     },
+     SearchAGroupButton: {
+       marginBottom: 15,
+     },
+
      MyGroupsText: {
        fontSize: 42,
        color: 'rgba(252, 249, 233, 1)',
        lineHeight: 40,
        textAlign: 'center',
        fontWeight: 'bold',
+       marginBottom: 10,
      },
      BoxIcon: {
+       resizeMode: 'contain',
        width: 200,
        height: 200,
-       resizeMode: 'contain',
+       marginTop: 20,
+       marginBottom: 20,
+       alignSelf: 'center',
+     },
+     navigationFilename: {
        marginTop: 5,
-       marginLeft: 5,
      },
 
    });

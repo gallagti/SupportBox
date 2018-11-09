99f961538515676ff665b553a51fdd9a2fe1b8e7
ssubrandon <brandon1380.bw@gmail.com>
2018-11-09 14:14:31 -0800

    changed something?

--- App.js
+++ App.js
@@ -7,7 +7,9 @@ import {root} from "native-base";
 export default class App extends React.Component {
   state = {
     isLoadingComplete: false,
+
     fontLoaded: false,
+
   };
 
   render() {
@@ -19,8 +21,10 @@ export default class App extends React.Component {
           onFinish={this._handleFinishLoading}
         />
       );
+
     }
     else {
+
       return (
         <View style={styles.container}>
           {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
@@ -42,8 +46,10 @@ export default class App extends React.Component {
         // We include SpaceMono because we use it in HomeScreen.js. Feel free
         // to remove this if you are not using it in your app
         'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+
         'Roboto': require("./node_modules/native-base/Fonts/Roboto.ttf"),
         'Roboto_medium': require("./node_modules/native-base/Fonts/Roboto_medium.ttf"),
+
       }),
     ]);
   };
